165185f2e7a71b6192e8cf09549620216841ba99
ironkayman <kostya.orlik@gmail.com>
2022-11-13 08:34:13 +0300

    WIP: keyboard UIWidget maping

--- alien_invasion/views/main_menu/main_menu.py
+++ alien_invasion/views/main_menu/main_menu.py
@@ -3,18 +3,9 @@ import arcade.gui
 
 from alien_invasion import CONSTANTS
 
-from alien_invasion.views import Invasion
-
-from alien_invasion.settings import KEYMAP
-
-
 from alien_invasion.views.invasion.sections.backgroud_engine import BackgroundEngine
 
-# from .sections import HumanInterface
-
-class QuitButton(arc.gui.UIFlatButton):
-    def on_click(self, event: arc.gui.UIOnClickEvent):
-        arc.exit()
+from .sections import HumanInterface
 
 class MainMenu(arc.View):
     """Main menu view."""
@@ -22,87 +13,36 @@ class MainMenu(arc.View):
     def __init__(self) -> None:
         super().__init__()
 
+        # self.background = arc.Sprite(
+        #     texture=arc.load_texture(
+        #         CONSTANTS.DIR_RESOURCES / 'images/background/main_menu.png'
+        #     ),
+        #     scale=0.95,
+        #     center_x=CONSTANTS.DISPLAY.WIDTH // 2 + 20,
+        #     center_y=160
+        # )
         self.background = arc.Sprite(
             texture=arc.load_texture(
-                CONSTANTS.DIR_RESOURCES / 'images/background/main_menu.png'
+                CONSTANTS.DIR_RESOURCES / 'images/background/entry_ns.png'
             ),
-            scale=0.95,
+            scale=0.85,
             center_x=CONSTANTS.DISPLAY.WIDTH // 2 + 20,
-            center_y=160
+            center_y=150
         )
 
-        self.background_engine = BackgroundEngine(
-            left=1000, bottom=0,
+        self.human_interface = HumanInterface(
+            left=0, bottom=0,
             width=self.window.width,
             height=self.window.height,
-            prevent_dispatch_view=False,
-            # prevent_dispatch={True},
-            # prevents events to propagate to the view
-            # prevent_dispatch_view={True},
-            # prevents arcade events capture
-            accept_keyboard_events=False,
-            name="background_engine",
+            name="human_interface",
         )
 
-        # self.human_interface = HumanInterface(
-        #     left=0, bottom=0,
-        #     width=self.window.width,
-        #     height=self.window.height,
-        #     name="human_interface",
-        # )
-
         # self.section_manager.add_section(self.background_engine)
-        # self.section_manager.add_section(self.human_interface, 444)
+        self.section_manager.add_section(self.human_interface, 444)
 
         self.window.set_mouse_visible(True)
 
-        # a UIManager to handle the UI
-        self.manager = arc.gui.UIManager(self.window)
-        self.manager.enable()
-
-        # Create a vertical BoxGroup to align buttons
-        self.v_box = arc.gui.UIBoxLayout()
-
-        # Create the buttons
-        start_button = arc.gui.UIFlatButton(text="Start Game", width=200)
-        self.v_box.add(start_button.with_space_around(bottom=20))
-        @start_button.event("on_click")
-        def on_click_start_invasion(event):
-            print("Starting...", event)
-            invasion_view = Invasion()
-            invasion_view.setup()
-            self.window.show_view(invasion_view)
-
-        # Again, method 1. Use a child class to handle events.
-        quit_button = QuitButton(text="Quit", width=200)
-        self.v_box.add(quit_button)
-
-        # Create a widget to hold the v_box widget, that will center the buttons
-        self.manager.add(
-            arc.gui.UIAnchorWidget(
-                anchor_x="center_x",
-                anchor_y="center_y",
-                child=self.v_box,
-            )
-        )
-
-
-
 
     def on_draw(self) -> None:
-        self.clear()
         arc.start_render()
         self.background.draw(pixelated=True)
-        self.manager.draw()
-        # print(arc.gui.UIEvent)
-
-
-    def on_key_press(self, symbol: int, modifiers: int) -> None:
-        if symbol == KEYMAP['start']:
-            print('starting ...')
-            invasion_view = Invasion()
-            invasion_view.setup()
-            self.window.show_view(invasion_view)
-        elif symbol == KEYMAP['quit']:
-            print('exitiing ...')
-            arc.exit()
--- alien_invasion/views/main_menu/sections/human_interface.py
+++ alien_invasion/views/main_menu/sections/human_interface.py
@@ -1,15 +1,38 @@
+from enum import IntEnum, auto
+
 import arcade as arc
+# why simple import?
 import arcade.gui
 
 from alien_invasion.constants import DISPLAY
-
+from alien_invasion.settings import KEYMAP
 from alien_invasion.views import Invasion
 
-from alien_invasion.settings import KEYMAP
+
+class EnumButton(IntEnum):
+    START = auto()
+    QUIT = auto()
+
 
 class QuitButton(arc.gui.UIFlatButton):
+    bid = EnumButton.QUIT
+
     def on_click(self, event: arc.gui.UIOnClickEvent):
+        print('Exiting ...')
         arc.exit()
+
+
+class StartButton(arc.gui.UIFlatButton):
+    bid = EnumButton.START
+
+    def on_click(self, event: arc.gui.UIOnClickEvent):
+        print("Starting ...", event)
+        invasion_view = Invasion()
+        invasion_view.setup()
+        # TODO: call prototype parent until window attr? functools.reduce?
+        root = self.parent.parent.parent
+        root.window.show_view(invasion_view)
+
 
 class HumanInterface(arc.Section):
 
@@ -30,50 +53,79 @@ class HumanInterface(arc.Section):
             height,
             **kwargs
         )
+
         # a UIManager to handle the UI
-        self.manager = arc.gui.UIManager(self.window)
+        self.manager = arc.gui.UIManager(self.view)
         self.manager.enable()
 
         # Create a vertical BoxGroup to align buttons
-        self.v_box = arc.gui.UIBoxLayout()
+        self.menu = arc.gui.UIBoxLayout()
 
-        # Create the buttons
-        start_button = arc.gui.UIFlatButton(text="Start Game", width=200)
-        self.v_box.add(start_button.with_space_around(bottom=20))
-        @start_button.event("on_click")
-        def on_click_start_invasion(event):
-            print("Starting...", event)
-            invasion_view = Invasion()
-            invasion_view.setup()
-            self.window.show_view(invasion_view)
+        start_button = StartButton(text="Start Game", width=200)
+        self.menu.add(start_button)#.with_space_around(bottom=20))
 
-        # Again, method 1. Use a child class to handle events.
         quit_button = QuitButton(text="Quit", width=200)
-        self.v_box.add(quit_button)
+        self.menu.add(quit_button)
 
-        # Create a widget to hold the v_box widget, that will center the buttons
+        # Create a widget to hold the menu widget, that will center the buttons
         self.manager.add(
             arc.gui.UIAnchorWidget(
                 anchor_x="center_x",
                 anchor_y="center_y",
-                child=self.v_box,
+                child=self.menu,
             )
         )
 
+        start_button.hovered = True
+        print('children:', self.menu.children)
+        print(self.selected_widget)
+
+    @property
+    def selected_widget(self) -> arc.gui.UIWidget|None:
+        return (
+            [*filter(
+                lambda wdg: getattr(wdg, 'hovered', False),
+                self.manager.walk_widgets()
+            )] or
+            [None]
+        )[0]
+
+    @selected_widget.setter
+    def selected_widget(self, widget: arc.gui.UIWidget|int) -> None:
+        found = None
+        for widget_from_tree in self.manager.walk_widgets():
+            # UIFlatButton untested
+            if (
+                isinstance(widget, arc.gui.UIFlatButton)
+                and widget_from_tree == widget
+            ):
+                found = widget_from_tree
+            elif (
+                isinstance(widget, int)
+                and (bid := getattr(widget_from_tree, 'bid', False))
+                and bid == widget
+            ):
+                found = widget_from_tree
+
+            if found:
+                # clean previous selection
+                if (selected:= self.selected_widget) is not None:
+                    selected.hovered = False
+                # activate new selection
+                found.hovered = True
+                break
+
+    def selected_widget_id(self) -> int:
+        return self.selected_widget.bid if self.selected_widget else -1
 
     def on_draw(self) -> None:
-        # arc.start_render()
-        # self.clear()
         self.manager.draw()
-        # print(arc.gui.UIEvent)
 
 
     def on_key_press(self, symbol: int, modifiers: int) -> None:
-        if symbol == KEYMAP['start']:
-            print('starting ...')
-            invasion_view = Invasion()
-            invasion_view.setup()
-            self.window.show_view(invasion_view)
-        elif symbol == KEYMAP['quit']:
-            print('exitiing ...')
-            arc.exit()
+        if symbol == arc.key.DOWN:
+            self.selected_widget = self.selected_widget_id() + 1
+        elif symbol == arc.key.UP:
+            self.selected_widget = self.selected_widget_id() - 1
+        elif symbol == arc.key.ENTER:
+            self.selected_widget.on_click(arc.gui.UIOnClickEvent)
