eaa31a3fcd064b258e460d4b0b6564d1454417ad
ironkayman <kostya.orlik@gmail.com>
2022-11-30 02:16:17 +0300

    WIP: added ItemWeapon class

--- alien_invasion/entities/items/item.py
+++ alien_invasion/entities/items/item.py
@@ -2,7 +2,10 @@ from abc import ABC
 
 from enum import IntEnum, auto
 
+from pydantic import BaseModel
+
 from alien_invasion.utils.loaders.item import load_item_as_dict
+
 
 class ItemType(IntEnum):
     ABC = auto()
@@ -10,13 +13,15 @@ class ItemType(IntEnum):
     ENGINE = auto()
     HULL = auto()
     THRUSTERS = auto()
-    WEAPONRY = auto()
+    WEAPON = auto()
 
 # move to pydadantic for field validation
 class Item(ABC):
     item_type: ItemType = ItemType.ABC
+    display_name: str
+    description: str
 
-    def __init__(self, model_name) -> None:
+    def __init__(self, model_name: str) -> None:
         self.item_name = model_name
         for k, v in load_item_as_dict(model_name).items():
             self.__setattr__(k, v)
@@ -59,3 +64,19 @@ class ItemHull(Item):
         for name in model_names:
             model = ItemArmor(name)
             self.__armor_models.append(model)
+
+class ItemWeapon(Item):
+    """
+    Attributes
+    ----------
+    energy_per_bullet : int
+        Enegry of ship's reactor consumed for a single bullet.
+    reload_speed : float
+        Time between bullet launches in `ms`.
+    """
+
+    item_type: ItemType = ItemType.WEAPON
+
+    energy_per_bullet: int
+    reload_speed: int
+
--- alien_invasion/utils/loaders/config/starship.py
+++ alien_invasion/utils/loaders/config/starship.py
@@ -4,18 +4,37 @@ from dataclasses import dataclass, field
 
 from alien_invasion.constants import DIR_EQUIPMENT
 
-from alien_invasion.entities.items.item import ItemArmor, ItemHull
+from alien_invasion.entities.items.item import ItemArmor, ItemHull, ItemWeapon
 
 
 @dataclass
 class StarshipStats:
     armor: int = field(default=0)
-
+    secondary_weapon_slots: int = field(default=0)
 class StarshipHull(ItemHull):
     def __init__(self, hull_dict: dict[str, Any]) -> None:
         super().__init__(hull_dict['model'])
         self.armor_models = hull_dict['armor']
 
+class StarshipWeaponry:
+    """
+    Attributes
+    ----------
+    primary : ItemWeapon
+        Primary starship's weapon.
+    secondaries : list[ItemWeapon]
+        List of secondary weapons.
+    """
+
+    primary: ItemWeapon
+    secondaries: list[ItemWeapon] = []
+
+    def __init__(self, weaponry_dict: dict[str, Any], hull: StarshipHull) -> None:
+        self.primary = ItemWeapon(weaponry_dict['primary']['model'])
+        if len(weaponry_dict['secondary']) > hull.secondary_weapon_mount_slots:
+            raise Exception('more than can be mounted')        
+        for sc in weaponry_dict['secondary']:
+            self.secondaries.append(ItemWeapon(sc['model']))
 
 class StarshipLoadout:
     """
@@ -28,9 +47,13 @@ class StarshipLoadout:
 
     def __init__(self, config) -> None:
         self.stats = StarshipStats()
+
         self.hull = StarshipHull(config['starship']['hull'])
         self.stats.armor = self.hull.total_armor
-        print(self.stats)
+        self.stats.secondary_weapon_slots = self.hull.secondary_weapon_mount_slots
+
+        self.weaponry = StarshipWeaponry(config['starship']['weaponry'], self.hull)
+
 
 
 def load_starship_loadout(config: dict):
