d71f84a67a8a7af01cf531bf8605ddd60070b122
ironkayman <kostya.orlik@gmail.com>
2023-03-12 04:04:14 +0400

    Aliens spread through spawner-emitter

--- alien_invasion/views/invasion/scenes/alien_area.py
+++ alien_invasion/views/invasion/scenes/alien_area.py
@@ -15,19 +15,19 @@ class AlienArea(arc.Scene):
     def __init__(self) -> None:
         super().__init__()
 
-        self.aliens: arc.SpriteList = arc.SpriteList()
+        self.aliens_types: arc.SpriteList = arc.SpriteList()
         self.aliens_bullet_list: arc.SpriteList = arc.SpriteList()
 
         alien_categories = loader()
         for config in alien_categories:
             alien = Alien(config)
-            self.aliens.append(alien)
+            self.aliens_types.append(alien)
 
         self.add_sprite_list(
-            name='aliens',
-            sprite_list=self.aliens,
+            name='aliens_types',
+            sprite_list=self.aliens_types,
         )
-        self.emitter = arc.Emitter(
+        self.spawner = arc.Emitter(
             center_xy=(CONSTANTS.DISPLAY.WIDTH // 2, CONSTANTS.DISPLAY.HEIGHT // 2),
             emit_controller=arc.EmitInterval(0.02),
             particle_factory=lambda emitter: Alien(
@@ -35,19 +35,25 @@ class AlienArea(arc.Scene):
                 change_xy= arc.rand_in_circle((0.0, 0.0), 10),
             )  # type: ignore
         )
+        # dont add sprite list to scene since spawner counts it
+        # self.add_sprite_list(
+        #     name='aliens',
+        #     sprite_list=self.spawner._particles,
+        # )
+        self.aliens = self.spawner._particles
 
     def on_update(self, delta_time: float = 1 / 60) -> None:
         """Compute background layer changes."""
 
-        if self.emitter:
-            self.emitter.update()
-        for alien in self.aliens:
-            alien.on_update(delta_time)
+        if self.spawner:
+            self.spawner.update()
+        print('aliens:', self.spawner.get_count())
+
 
 
     def draw(self):
         """
         Render background section.
         """
-        self.emitter.draw()
+        self.spawner.draw()
         super().draw(pixelated=True)
