d4a2eed542246fa23fa80c894ad25661eb324588
ironkayman <kostya.orlik@gmail.com>
2023-03-17 02:44:16 +0400

    AlienSpawner docstring

--- alien_invasion/views/invasion/scenes/level/spawner.py
+++ alien_invasion/views/invasion/scenes/level/spawner.py
@@ -1,9 +1,15 @@
+"""Logic for Alien spawners
+"""
+
 from typing import cast
 
 import arcade as arc
+from arcade import Particle
 
 
 class AlienSpawner(arc.Emitter):
+    """Implementation of Emitter specifically for Alien class particle.
+    """
 
     def __init__(
         self,
@@ -12,6 +18,8 @@ class AlienSpawner(arc.Emitter):
         return super().__init__(**emitter_kwargs)
 
     def on_update(self, delta_time):
+        """Impliments on_update method on par with standard .update
+        """
         # update emitter
         self.center_x += self.change_x
         self.center_y += self.change_y
@@ -21,7 +29,8 @@ class AlienSpawner(arc.Emitter):
         emit_count = self.rate_factory.how_many(1 / 60, len(self._particles))
         for _ in range(emit_count):
             self._emit()
+        # notice
         self._particles.on_update(delta_time)
-        particles_to_reap = [p for p in self._particles if cast(arc.Particle, p).can_reap()]
+        particles_to_reap = [p for p in self._particles if cast(Particle, p).can_reap()]
         for dead_particle in particles_to_reap:
             dead_particle.kill()
