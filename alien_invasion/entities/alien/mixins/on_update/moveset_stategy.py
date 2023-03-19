760c7e92417c7b4ebd77b785043b84cec013a7b1
ironkayman <kostya.orlik@gmail.com>
2023-03-20 03:02:42 +0400

    Alien proximity multiplier changed to 3.3

--- alien_invasion/entities/alien/mixins/on_update/moveset_stategy.py
+++ alien_invasion/entities/alien/mixins/on_update/moveset_stategy.py
@@ -1,3 +1,4 @@
+from random import random
 from typing import cast
 import math
 
@@ -31,7 +32,7 @@ def on_update_plot_movement(self, delta_time) -> None:
 
     if AlienMoveset.tracking in movesets:
         if not self.dodging:
-            if math.fabs(self.center_x - ship_x) < self._starship.width / 2:
+            if math.fabs(self.center_x - ship_x) < self._starship.width / 3.3: # 2 allow not to be hit
                 self.change_x = 0
             elif self.center_x > ship_x:
                 self._timers.reset_track()
