44af5a485f80531da88679ed872584f34230908c
ironkayman <kostya.orlik@outlook.com>
2023-08-26 19:46:31 +0400

    Fixed alien movement freezes behind starship

--- alien_invasion/entities/alien/mixins/on_update/moveset_stategy.py
+++ alien_invasion/entities/alien/mixins/on_update/moveset_stategy.py
@@ -11,16 +11,12 @@ from alien_invasion.entities.common.state_manager.state import AlienMoveset
 from alien_invasion.entities.starship import Starship
 
 
-def on_update_plot_movement(
-        alien,
-        starship: Starship,
-        delta_time: float
-    ) -> None:
-    """Based on current moveset and ship's posirion calculate its movement
-    """
+def on_update_plot_movement(alien, starship: Starship, delta_time: float) -> None:
+    """Based on current moveset and ship's posirion calculate its movement"""
 
     # workaround circular imports
     from ...alien import Alien
+
     alien = cast(Alien, alien)
 
     def get_alien_count_proportion_on_x_axis() -> float:
@@ -33,18 +29,31 @@ def on_update_plot_movement(
             on the right side of the current entity,
             divided by same count on the right.
         """
-        aliens_count_on_right = len(tuple(filter(
-            lambda a: a.center_x > alien.center_x,
-            alien._parent_sprite_list
-        ))) or 1
-        aliens_count_on_left = len(tuple(filter(
-            lambda a: a.center_x < alien.center_x,
-            alien._parent_sprite_list
-        ))) or 1
+        aliens_count_on_right = (
+            len(
+                tuple(
+                    filter(
+                        lambda a: a.center_x > alien.center_x, alien._parent_sprite_list
+                    )
+                )
+            )
+            or 1
+        )
+        aliens_count_on_left = (
+            len(
+                tuple(
+                    filter(
+                        lambda a: a.center_x < alien.center_x, alien._parent_sprite_list
+                    )
+                )
+            )
+            or 1
+        )
         # >0 - left more, <0 - right more
         return aliens_count_on_left / aliens_count_on_right
 
-    if starship.can_reap(): return
+    if starship.can_reap():
+        return
 
     # configure movement based on state's movesets
     movesets = alien.state.movesets
@@ -52,12 +61,8 @@ def on_update_plot_movement(
     relative_amount = get_alien_count_proportion_on_x_axis()
 
     # when theresa persuing moveset, stop right above center of the veiwport
-    if (
-        (
-            AlienMoveset.persuing in movesets or alien._overrides.should_persue
-        ) and (
-            alien.bottom < CONSTANTS.DISPLAY.HEIGHT * 0.6
-        )
+    if (AlienMoveset.persuing in movesets or alien._overrides.should_persue) and (
+        alien.bottom < CONSTANTS.DISPLAY.HEIGHT * 0.6
     ):
         alien.change_y = 0
 
@@ -65,7 +70,10 @@ def on_update_plot_movement(
 
     if AlienMoveset.tracking in movesets:
         if not alien.dodging:
-            if math.fabs(alien.center_x - ship_x) < starship.width / starship.width * random():
+            if (
+                math.fabs(alien.center_x - ship_x)
+                < starship.width / starship.width * random()
+            ):
                 alien.change_x = 0
             elif alien.center_x > ship_x:
                 alien._timers.reset_track()
@@ -79,10 +87,7 @@ def on_update_plot_movement(
 
         # stop movement when reaching borders with Bordered moveset property
         if is_bordered:
-            if (
-                alien.center_x <= 0 or
-                alien.center_x >= CONSTANTS.DISPLAY.WIDTH
-            ):
+            if alien.center_x <= 0 or alien.center_x >= CONSTANTS.DISPLAY.WIDTH:
                 alien.change_x *= -1
 
     elif AlienMoveset.escaping in movesets:
@@ -92,7 +97,3 @@ def on_update_plot_movement(
             alien.change_x = alien.speed * delta_time
         elif alien.center_x < ship_x:
             alien.change_x = -alien.speed * delta_time
-
-    # if alien reaches ship's top, dont change alien's x-axis
-    if alien.center_y < starship.top * 1.33:
-        alien.change_x = 0
